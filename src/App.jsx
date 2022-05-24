ce73cc49ba18d4fbda2e7771172108585741a37e
Thomas Ravache <thomasravache31@gmail.com>
2022-05-24 17:45:37 -0300

    feat: utilizar actionCreators no código

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './state';
+
+function App() {
+  const account = useSelector((state) => state.account);
+  const dispatch = useDispatch();
+
+  const { depositMoney, withdrawMoney } = bindActionCreators(actionCreators, dispatch);
+
+  console.log(account);
+
+  return (
+    <div className="App">
+      <button onClick={() => depositMoney(10)}>Depositar 10</button>
+      <button onClick={() => withdrawMoney(5)}>Sacar 5</button>
+      <h1>{account}</h1>
+    </div>
+  );
+}
+
+export default App;
