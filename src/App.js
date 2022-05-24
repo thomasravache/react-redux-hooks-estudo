d0fa96cf4172840cdb81484ca7b8355ce0943c31
Thomas Ravache <thomasravache31@gmail.com>
2022-05-24 13:32:05 -0300

    feat: mostrar valor do estado do redux

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import { useSelector } from 'react-redux';
 
 function App() {
-  const state = useSelector((state) => state);
+  const account = useSelector((state) => state.account);
 
-  console.log(state);
+  console.log(account);
 
   return (
     <div className="App">
--- src/state/store.js
+++ src/state/store.js
@@ -1,14 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducers from './reducers';
-import accountReducer from './reducers/accountReducer';
-
-const reducer = {
-  account: accountReducer,
-  //outro: outroReducer,
-}
 
 const store = configureStore({
-  reducer,
+  reducer: reducers,
   preloadedState: {},
 });
 
