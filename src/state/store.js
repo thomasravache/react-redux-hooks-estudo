3ffa67781cb3a4b53c849d956f1a713c973731c9
Thomas Ravache <thomasravache31@gmail.com>
2022-05-24 13:23:57 -0300

    feat: mostrar criação store sem combineReducer

--- src/state/store.js
+++ src/state/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducers from './reducers';
+import accountReducer from './reducers/accountReducer';
+
+const reducer = {
+  account: accountReducer,
+  //outro: outroReducer,
+}
 
 const store = configureStore({
-  reducer: reducers,
+  reducer,
+  preloadedState: {},
 });
 
 export default store;
