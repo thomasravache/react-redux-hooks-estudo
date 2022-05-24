9132e0ea649f4a6405b220cf2c0b9ea9422f27cd
Thomas Ravache <thomasravache31@gmail.com>
2022-05-24 18:13:03 -0300

    feat: testar assincronicidade nos action-creators

--- src/state/action-creators/index.js
+++ src/state/action-creators/index.js
@@ -1,5 +1,12 @@
+const promise = new Promise((resolve, reject) => {
+  resolve(1);
+})
+
 export const depositMoney = (amount) => {
   return async (dispatch) => {
+    const value = await promise;
+    console.log(value);
+    amount = value;
     dispatch({
       type: 'DEPOSIT',
       payload: amount,
